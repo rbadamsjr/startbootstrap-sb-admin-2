a23d52290be2351ea1bcad4c5c74f2031ec9ffa3
Roland Adams <roland.adams@smartideasllc.net>
2017-02-01 00:02:42 -0500

    still working

--- angular/app/app.module.js
+++ angular/app/app.module.js
@@ -5,6 +5,7 @@ var sbAdminApp = angular.module('app', ['ngRoute','ui.bootstrap']);
 // Routing
 sbAdminApp.config(['$routeProvider',
 	function($routeProvider) {
+		console.log($routeProvider);
 	 $routeProvider.
 		when('/', {
 		  templateUrl: 'app/views/dashboard.html',
@@ -22,11 +23,17 @@ sbAdminApp.config(['$routeProvider',
 
   //Controllers
 	sbAdminApp.controller('LoginCtrl', function($scope) {
-		
+		console.log('LoginCtrl');
 	});
 
 	sbAdminApp.controller('DashboardCtrl', function($scope) {
+		console.log('DashboardCtrl');
+	//	$('#side-menu').metisMenu();	
+	});
 
+	sbAdminApp.controller('SideNavCtrl', function($scope) {
+		console.log('SideNavCtrl');
+		$('#side-menu').metisMenu();
 	});
 
 })();
